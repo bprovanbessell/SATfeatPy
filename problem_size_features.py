41187fbae9aa3010e1d0c7998929cbe529e4980f
Benjamin Provan-Bessell <benjaminlpb123@gmail.com>
2021-12-10 15:48:53 +0000

    Get size features

--- problem_size_features.py
+++ problem_size_features.py
@@ -1,2 +1,15 @@
 import cnfgen
 
+with open("cnf_examples/basic.cnf") as f:
+    for line in f:
+        if line[0] == 'c':
+            continue
+        if line[0] == 'p':
+            sizes = line.split(" ")
+            variables = int(sizes[2])
+            clauses = int(sizes[3])
+            ratio = clauses/variables
+
+            print(clauses)
+            print(variables)
+            print(ratio)
