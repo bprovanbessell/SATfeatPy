38630a11294beeb24e5cf8d7ab9ec61f982dd7cb
Benjamin Provan-Bessell <benjaminlpb123@gmail.com>
2022-01-13 11:32:37 +0000

    Add basis for dpll code

--- feature_computation/dpll.py
+++ feature_computation/dpll.py
@@ -1,7 +1,96 @@
+import random
+
 import parse_cnf
+
+def get_counter(formula):
+    counter = {}
+    for clause in formula:
+        for literal in clause:
+            if literal in counter:
+                counter[literal] += 1
+            else:
+                counter[literal] = 1
+    return counter
+
+
+def bcp(formula, unit):
+    """
+    If the unit is in the clause, it is fine, as it can be satisfied
+    If a negative unit is in there, then remove it from the clause, as this will not be satisfied
+
+    :param formula: formula (cnf still left to solve)
+    :param unit:
+    :return:
+    """
+    modified = []
+    for clause in formula:
+        # this clause is satisfied, so we don't need to search anywhere in it anymore
+        if unit in clause: continue
+        if -unit in clause:
+            c = [x for x in clause if x != -unit]
+            # clause is unsatisfiable
+            if len(c) == 0: return -1
+            modified.append(c)
+        else:
+            modified.append(clause)
+    return modified
+
+
+def unit_propagation(formula):
+    assignment = []
+    unit_clauses = [c for c in formula if len(c) == 1]
+    while len(unit_clauses) > 0:
+        unit = unit_clauses[0]
+        formula = bcp(formula, unit[0])
+        assignment += [unit[0]]
+        if formula == -1:
+            return -1, []
+        if not formula:
+            return formula, assignment
+        unit_clauses = [c for c in formula if len(c) == 1]
+    return formula, assignment
+
+
+
+def pure_literal(formula):
+    counter = get_counter(formula)
+    assignment = []
+    pures = []
+    for literal, num in counter.items():
+        if -literal not in counter:
+            pures.append(literal)
+    for pure in pures:
+        formula = bcp(formula, pure)
+    assignment += pures
+    return formula, assignment
+
+
+def variable_selection(formula, assignment):
+    counter = get_counter(formula)
+    return random.choice(counter.keys())
+
+
+def backtracking(formula, assignment):
+    formula, pure_assignment = pure_literal(formula)
+    formula, unit_assignment = unit_propagation(formula)
+
+    assignment = assignment + pure_assignment + unit_assignment
+    if formula == -1:
+        return []
+    if not formula:
+        return assignment
+
+    # randomly choose a variable to set
+    variable = variable_selection(formula)
+    solution = backtracking(bcp(formula, variable), assignment + [variable])
+    if not solution:
+        solution = backtracking(bcp(formula, -variable), assignment + [-variable])
+    return solution
 
 if __name__ == "__main__":
     # test out the algo here
-    cnf_path = "cnf_examples/basic.cnf"
+    cnf_path = "../cnf_examples/basic.cnf"
 
-    parse_cnf.parse_cnf()
+    clauses, c, v = parse_cnf.parse_cnf()
+
+    solution = backtracking(clauses, [])
