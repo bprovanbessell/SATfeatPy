1397906524521ddb09277a99725e33f193bc420a
Benjamin Provan-Bessell <benjaminlpb123@gmail.com>
2022-01-13 11:32:19 +0000

    Clean up code a bit

--- feature_computation/active_features.py
+++ feature_computation/active_features.py
@@ -1,6 +1,8 @@
 """
-The features as described in the SATzilla paper are different to those in the code.
 First we need to compute the active variables and clauses
+After pre-processing, active variable and clause computation is done
+I'm not sure how redundant this is, possibly the SatELite pre-processing takes care of most of this...
+Tautologies are removed,
 """
 
 
@@ -60,13 +62,7 @@ def get_active_features(clauses, c, v):
                 num_ternary_clauses += 1
 
             for i in range(num_literals):
-                # if (lits[litNum] < 0)
-                #     negClausesWithVar[ABS(lits[litNum])].push_back(clauseNum);
-                #     else
-                #     posClausesWithVar[lits[litNum]].push_back(clauseNum);
 
-                # print(i)
-                # print(num_active_clauses_with_var)
                 num_active_clauses_with_var[abs(clause[i])] += 1
 
         else:
@@ -85,45 +81,4 @@ def get_active_features(clauses, c, v):
 
     return num_active_vars, num_active_clauses, clause_states, clauses
 
-
-
-
-    # for clause_num, clause in enumerate(clauses):
-    #     num_literals = len(clause)
-    #
-    #     # test if some literals are redundant and sort the clause
-    #     tautology = False
-    #
-    #     for i in range(num_literals - 1):
-    #         temp_literal = clause[i]
-    #
-    #         for j in range(i + 1, num_literals):
-    #             # this is sorting the literals in the clause
-    #
-    #             if abs(temp_literal) > abs(clause[j]):
-    #                 temp = clause[j]
-    #                 clause[j] = temp_literal
-    #                 temp_literal = temp
-    #
-    #             elif temp_literal == clause[j]:
-    #                 newj = j - 1
-    #                 num_literals -=1
-    #
-    #                 clause[newj] = clause[num_literals]
-    #
-    #                 # lits[j --] = lits[--numLits];
-    #                 # printf("c literal %d is redundant in clause %d\n", tempLit, clauseNum);
-    #                 # redundant literal, should be skipped
-    #                 pass
-    #             elif abs(temp_literal) == abs(clause[j]):
-    #                 # tautology means it is always true
-    #                 tautology = True
-    #
-    #         if tautology:
-    #             break
-    #         else:
-    #             clause[i] = temp_literal
-    #
-    #     if not tautology:
-    #         clause_states[clause_num] = True
 
