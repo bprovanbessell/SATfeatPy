f9ac7f8db4e6c94c731288d58412eb3c9900344c
Benjamin Provan-Bessell <benjaminlpb123@gmail.com>
2022-01-14 10:32:04 +0000

    Finish basic test

--- tests/satzilla_comparison_tests.py
+++ tests/satzilla_comparison_tests.py
@@ -8,45 +8,48 @@ class SatzillaComparisonTest(unittest.TestCase):
     Satzilla and satelite only run on linux unfortunately
     """
 
-    # mapping of the satzilla names for features
-    satzilla_names_map = {
-        "nvars": "v",
-        "nclauses": "c",
-        "vars-clauses-ratio": "vars_clauses_ratio",
-        "POSNEG-RATIO-CLAUSE-mean": "pnc_ratio_mean",
-        "POSNEG-RATIO-CLAUSE-coeff-variation": "pnc_ratio_coeff",
-        "POSNEG-RATIO-CLAUSE-min": "pnc_ratio_min",
-        "POSNEG-RATIO-CLAUSE-max": "pnc_ratio_max",
-        "POSNEG-RATIO-CLAUSE-entropy": "pnc_ratio_entropy",
-        "VCG-CLAUSE-mean": "vcg_clause_mean",
-        "VCG-CLAUSE-coeff-variation": "vcg_clause_coeff",
-        "VCG-CLAUSE-min": "vcg_clause_min",
-        "VCG-CLAUSE-max": "vcg_clause_max",
-        "VCG-CLAUSE-entropy": "vcg_clause_entropy",
-        # "UNARY": ,
-        "BINARY+": "binary_ratio",
-        "TRINARY+": "ternary+",
-        "VCG-VAR-mean": "vcg_var_mean",
-        "VCG-VAR-coeff-variation": "vcg_var_coeff",
-        "VCG-VAR-min": "vcg_var_min",
-        "VCG-VAR-max": "vcg_var_max",
-        "VCG-VAR-entropy": "vcg_var_entropy",
-        "POSNEG-RATIO-VAR-mean": "pnv_ratio_mean",
-        "POSNEG-RATIO-VAR-stdev": "pnv_ratio_stdev",
-        "POSNEG-RATIO-VAR-min": "pnv_ratio_min",
-        "POSNEG-RATIO-VAR-max": "pnv_ratio_max",
-        "POSNEG-RATIO-VAR-entropy": "pnv_ratio_entropy",
-        "HORNY-VAR-mean": "hc_var_mean",
-        "HORNY-VAR-coeff-variation": "hc_var_coeff",
-        "HORNY-VAR-min": "hc_var_min",
-        "HORNY-VAR-max": "hc_var_max",
-        "HORNY-VAR-entropy": "hc-var_entropy",
-        "horn-clauses-fraction": "hc_fraction",
-        "VG-mean": "vg_mean",
-        "VG-coeff-variation": "vg_coeff",
-        "VG-min": "vg_min",
-        "VG-max": "vg_max"
-    }
+    def __init__(self):
+
+        # mapping of the satzilla names for features
+        self.satzilla_names_map = {
+            "nvars": "v",
+            "nclauses": "c",
+            "vars-clauses-ratio": "vars_clauses_ratio",
+            "POSNEG-RATIO-CLAUSE-mean": "pnc_ratio_mean",
+            "POSNEG-RATIO-CLAUSE-coeff-variation": "pnc_ratio_coeff",
+            "POSNEG-RATIO-CLAUSE-min": "pnc_ratio_min",
+            "POSNEG-RATIO-CLAUSE-max": "pnc_ratio_max",
+            "POSNEG-RATIO-CLAUSE-entropy": "pnc_ratio_entropy",
+            "VCG-CLAUSE-mean": "vcg_clause_mean",
+            "VCG-CLAUSE-coeff-variation": "vcg_clause_coeff",
+            "VCG-CLAUSE-min": "vcg_clause_min",
+            "VCG-CLAUSE-max": "vcg_clause_max",
+            "VCG-CLAUSE-entropy": "vcg_clause_entropy",
+            # "UNARY": ,
+            "BINARY+": "binary_ratio",
+            "TRINARY+": "ternary+",
+            "VCG-VAR-mean": "vcg_var_mean",
+            "VCG-VAR-coeff-variation": "vcg_var_coeff",
+            "VCG-VAR-min": "vcg_var_min",
+            "VCG-VAR-max": "vcg_var_max",
+            "VCG-VAR-entropy": "vcg_var_entropy",
+            "POSNEG-RATIO-VAR-mean": "pnv_ratio_mean",
+            "POSNEG-RATIO-VAR-stdev": "pnv_ratio_stdev",
+            "POSNEG-RATIO-VAR-min": "pnv_ratio_min",
+            "POSNEG-RATIO-VAR-max": "pnv_ratio_max",
+            "POSNEG-RATIO-VAR-entropy": "pnv_ratio_entropy",
+            "HORNY-VAR-mean": "hc_var_mean",
+            "HORNY-VAR-coeff-variation": "hc_var_coeff",
+            "HORNY-VAR-min": "hc_var_min",
+            "HORNY-VAR-max": "hc_var_max",
+            "HORNY-VAR-entropy": "hc-var_entropy",
+            "horn-clauses-fraction": "hc_fraction",
+            "VG-mean": "vg_mean",
+            "VG-coeff-variation": "vg_coeff",
+            "VG-min": "vg_min",
+            "VG-max": "vg_max"
+        }
+
     def test_base_features(self):
         os.chdir("../SAT-features-competition2012/")
         satzilla_results_file = "output_base_feat"
@@ -88,20 +91,23 @@ class SatzillaComparisonTest(unittest.TestCase):
         preprocessed_path = "cnf_examples/out.cnf"
         features_dict = features.compute_features_from_file(preprocessed_path)
 
+        for sat_feat_name, feat_name in self.satzilla_names_map.items():
+            self.assertAlmostEqual(satzilla_features[sat_feat_name], features_dict[feat_name])
 
 
-
-os.chdir("../SAT-features-competition2012/")
-stream = os.popen('./features --base basic.cnf')
-output = stream.read()
-
-output = output.split("\n")
-
-features_names = output[-3].split(',')
-features = map(float, output[-2].split(','))
-
-features_dict = dict(zip(features_names, features))
-
-print("feature names: ", features_names)
-print("features: ", features)
-print(features_dict)
+#
+#
+# os.chdir("../SAT-features-competition2012/")
+# stream = os.popen('./features --base basic.cnf')
+# output = stream.read()
+#
+# output = output.split("\n")
+#
+# features_names = output[-3].split(',')
+# features = map(float, output[-2].split(','))
+#
+# features_dict = dict(zip(features_names, features))
+#
+# print("feature names: ", features_names)
+# print("features: ", features)
+# print(features_dict)
