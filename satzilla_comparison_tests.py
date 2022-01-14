2c09dbdf878a51cc92381f39be44e2d508770576
Benjamin Provan-Bessell <benjaminlpb123@gmail.com>
2022-01-14 12:03:31 +0000

    ditto

--- satzilla_comparison_tests.py
+++ satzilla_comparison_tests.py
@@ -1,8 +1,8 @@
 import os
 import sys
-sys.path.append("/Users/bprovan/Insight/SAT-features")
+sys.path.append("/")
 sys.path.append("/home/bprovan/Insight/SAT-features")
-# print(sys.path)
+print(sys.path)
 import unittest
 from feature_computation import parse_cnf, balance_features, graph_features, array_stats, active_features, preprocessing
 import features as main_features
@@ -53,7 +53,7 @@ class SatzillaComparisonTest(unittest.TestCase):
             "VG-max": "vg_max"
         }
 
-        os.chdir("../../SAT-features-competition2012/")
+        os.chdir("../SAT-features-competition2012/")
         satzilla_results_file = "output_base_feat"
         input_cnf_file = "basic.cnf"
 
@@ -82,7 +82,7 @@ class SatzillaComparisonTest(unittest.TestCase):
 
         f.close()
 
-        os.chdir("..")
+        os.chdir("")
         # compute the features with our code
         # preprocess the file with satelite
         cnf_path = "cnf_examples/" + input_cnf_file
@@ -90,7 +90,7 @@ class SatzillaComparisonTest(unittest.TestCase):
 
         # n.b. satelite only works on linux, mac no longer supports 32 bit binaries...
         preprocessing.satelite_preprocess(cnf_path)
-        preprocessed_path = "../cnf_examples/out.cnf"
+        preprocessed_path = "cnf_examples/out.cnf"
         features_dict = main_features.compute_features_from_file(preprocessed_path)
 
         for sat_feat_name, feat_name in self.satzilla_names_map.items():
@@ -98,7 +98,7 @@ class SatzillaComparisonTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    os.chdir("..")
+    # os.chdir("")
     unittest.main()
 #
 #
